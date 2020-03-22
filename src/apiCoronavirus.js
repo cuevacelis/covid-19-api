39ef50c58fa3b9172561545c3f39c577f4765618
cuevacelis <cuevacelis@hotmail.com>
2020-03-21 21:32:15 -0500

    algunas mejoras asincronas

--- src/apiCoronavirus.js
+++ src/apiCoronavirus.js
@@ -1,27 +1,26 @@
-import path from "path";
-import express from "express";
-import cors from "cors";
-import expressIp from "express-ip";
+var compression = require('compression')
+var express = require("express")
+var cors = require("cors")
 import routers from "./routers";
-
-const app = express();
+var expressIp = require("express-ip")
+var app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(express.json());
 app.use(expressIp().getIpInfoMiddleware);
 app.use(cors({ origin: "*" }));
-
 app.use(routers);
 
-app.get("/api/coronavirus", (req, res) => {
-  res.send({
+app.get("/api/coronavirus",async (req, res) => {
+  await res.send({
     developer: "Jose Cueva Celis",
     linkedin: "https://www.linkedin.com/in/cuevacelis/",
   });
 });
 
-app.get("*", (req, res) => {
-  res.sendStatus(404);
+app.get("*",async (req, res) => {
+  await res.sendStatus(404);
 });
 
 app.listen(port, () => {
