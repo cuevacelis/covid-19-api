08a59714ed1ce01d54c983cc95a7f6b8b9cb47d9
cuevacelis <cuevacelis@hotmail.com>
2023-05-09 14:08:57 -0500

    fix: lambda

--- src/models/corona.js
+++ src/models/corona.js
@@ -2,21 +2,11 @@ const { getTotal, getCountries } = require("../db/index.js");
 
 class Corona {
   async total() {
-    try {
-      const dataTotal = await getTotal();
-      return dataTotal;
-    } catch (error) {
-      throw error;
-    }
+    return await getTotal();
   }
 
   async countries() {
-    try {
-      const dataCountries = await getCountries();
-      return dataCountries;
-    } catch (error) {
-      throw error;
-    }
+    return await getCountries();
   }
 }
 
